0c0b4966f1b1849029d330231dff6814d9d16e14
Egor Shramko <egorii2001@gmail.com>
2026-01-15 16:58:54 +0700

    First commit

---
+++ src/main/java/com/example/jenkinsdemo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.jenkinsdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String homePage() {
+        return "home";
+    }
+
+}
